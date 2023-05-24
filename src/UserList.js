e7636a22253db915f2ee476427d630e08ce1da1f
HodaSalim <hi@hodasalim.com>
2023-05-24 16:12:27 +0300

    completed project with no css

---
+++ src/UserList.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export function UserList() {
+  const [listOfUsers, setListOfUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setListOfUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div>
+      {listOfUsers.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
